7b3a31d35408378309c6b4a710106af1237655dc
forhadisrafil <forhadtuhin5088@gmail.com>
2022-10-27 00:24:47 +0600

    init

---
+++ read_xml.py
@@ -0,0 +1,64 @@
+import xmltodict
+import json
+
+
+def get_xml_data(xml_file_path):
+    file = open(xml_file_path)
+    xml_att = xmltodict.parse(file.read())
+    print(xml_file_path, )
+
+    # with open("d.json", 'w') as d:
+    #     d.write(json.dumps(xml_att))
+    body_control = []
+    tire_pressure = []
+
+    for data in xml_att['VDoc']['VDocSection']['DataItems']:
+        if data['@attributeName'] == "ActionSelection":
+            vehicle_info = data['Item']
+            vehicle_info.append({'@name': 'VIN', '@value': xml_att['VDoc']['@VehicleIdentificationNumber']})
+            vehicle_info.append({'@name': 'ODOMETER', '@value': xml_att['VDoc']['@Odometer']})
+            vehicle_info.append({'@name': 'LICENSE PLATE', '@value': xml_att['VDoc']['@LicensePlateNumber']})
+            vehicle_info.append({'@name': 'DateCreated', '@value': xml_att['VDoc']['@DateCreated']})
+            # print(vehicle_info)
+        if "CodeScanSystems" in data:
+            # print(data)
+            CodeScanSystems = data['CodeScanSystems']
+            # print(CodeScanSystems)
+            for system in CodeScanSystems['System']:
+                if system['@name'] == 'Body Control Module':
+                    try:
+                        body_control = system['Item']
+                        if isinstance(body_control, dict):
+                            body_control = [system['Item']]
+                    except Exception as e:
+                        body_control = []
+
+                    # print(body_control)
+                if system['@name'] == 'Tire Pressure Monitor':
+                    try:
+                        tire_pressure = system['Item']
+                        if isinstance(tire_pressure, dict):
+                            tire_pressure = [system['Item']]
+                    except Exception as e:
+                        tire_pressure = []
+                    # print(tire_pressure)
+
+        # readiness
+        if "ReadinessMonitorsTestsComplete" in data:
+            # print(data['ReadinessMonitorsTestsComplete'])
+            readiness = data['ReadinessMonitorsTestsComplete']['Item']
+        if "CodeScanType" in data:
+            code_scan_type = data['CodeScanType']['@value']
+
+    top_information = {}
+    for vehicle in vehicle_info:
+        if vehicle["@name"] == 'YEAR':
+            top_information['year'] = vehicle
+        if vehicle["@name"] == 'MAKE':
+            top_information['make'] = vehicle
+        if vehicle["@name"] == 'MODEL':
+            top_information['model'] = vehicle
+        if vehicle["@name"] == 'ENGINE':
+            top_information['engine'] = vehicle
+
+    return vehicle_info, CodeScanSystems, body_control, tire_pressure, readiness, code_scan_type, top_information
---
+++ xml_data_to_pdf.py
@@ -0,0 +1,95 @@
+import jinja2
+from xhtml2pdf import pisa
+from read_xml import get_xml_data
+from glob import glob
+import os
+import shutil
+import time
+
+
+# =============================================
+
+# Utility function
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+        src=source_html,  # the HTML to convert
+        dest=result_file,  # file handle to recieve result
+    )
+
+    # close output file
+    result_file.close()  # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+
+# ===================================
+
+
+# Main program
+if __name__ == "__main__":
+    try:
+        os.mkdir("PDF_Files")
+    except Exception as e:
+        pass
+    try:
+        os.mkdir("html_files")
+    except Exception as e:
+        pass
+    # ====================== pdf ========================
+    pisa.showLogging()
+    templateLoader = jinja2.FileSystemLoader(searchpath=".")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "pdf_report.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    # todo: specify the folder of xml files
+    xmls = glob("all_xml_files/*.XML")
+    for xml_file_path in xmls:
+        output_filename = xml_file_path.split("\\")[1].split(".")[0]
+
+        vehicle_infos, CodeScanSystems, body_controls, tire_pressures, readinesses, code_scan_type, top_information = get_xml_data(
+            xml_file_path)
+
+        output_text = template.render(
+            vehicle_infos=vehicle_infos,
+            CodeScanSystems=CodeScanSystems,
+            body_controls=body_controls,
+            tire_pressures=tire_pressures,
+            readinesses=readinesses,
+            code_scan_type=code_scan_type,
+            top_information=top_information,
+        )
+
+        with open("html_files/" + output_filename + ".html", 'w') as html_file:
+            html_file.write(output_text)
+            html_file.close()
+
+        for vehicle in vehicle_infos:
+            if vehicle["@name"] == 'VIN':
+                pdf_name = vehicle["@value"][-8:] + '_' + code_scan_type
+
+        source_html = open("html_files/" + output_filename + ".html", "r+b")
+        output_filename = "PDF_Files/" + pdf_name + ".pdf"
+
+        convert_html_to_pdf(source_html, output_filename)
+
+    # ====================== pdf ========================
+
+'''
+I need a script to change xml to pdf it’ll be the same format
+and the file name should be last 8 of the vin + “pre” or “post”
+
+issue: 
+1. you’re missing all the top left information on the front page==done
+2. you’re also missing the document headers==not possible currently
+3. Also missing the last 9 of the VIN’s Characters on the end of each file
+, the script to name each file last 8 characters of the VIN (Veichle Identification Number) + Post or Pre
+
+4. See how this tag is “Pre” or “Post” this needs to get amended to the end of each PDF==done
+
+'''
